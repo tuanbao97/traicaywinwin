bafac5865215e5a35b38bd7bea80e775af7cfe8b
unknown <tuanbao97@gmail.com>
2026-07-15 09:38:59 +0700

    Update

--- resources/views/UI-FRONTEND/common/theme-head-styles.blade.php
+++ resources/views/UI-FRONTEND/common/theme-head-styles.blade.php
@@ -36,8 +36,8 @@
     <link rel="stylesheet" href="100/531/894/themes/1018832/assets/icon.css?1768901692132" media="all">
     <link rel="preload" as="style" media="all" href="100/531/894/themes/1018832/assets/global.css?ww-scroll-mid-1">
     <link rel="stylesheet" href="100/531/894/themes/1018832/assets/global.css?ww-scroll-mid-1" media="all">
-    <link rel="preload" as="style" media="all" href="100/531/894/themes/1018832/assets/custom.css?ww-grid-mobile-3-1">
-    <link rel="stylesheet" href="100/531/894/themes/1018832/assets/custom.css?ww-grid-mobile-3-1" media="all">
+    <link rel="preload" as="style" media="all" href="100/531/894/themes/1018832/assets/custom.css?ww-scroll-pad-5">
+    <link rel="stylesheet" href="100/531/894/themes/1018832/assets/custom.css?ww-scroll-pad-5" media="all">
     <link
       rel="stylesheet"
       href="100/531/894/themes/1018832/assets/quickview.css?ww-scroll-mid-1"
