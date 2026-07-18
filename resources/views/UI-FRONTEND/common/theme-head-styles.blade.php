efb26dff0c294039329431cfe4cfd0beac0646ca
unknown <tuanbao97@gmail.com>
2026-07-18 18:57:44 +0700

    Update

--- resources/views/UI-FRONTEND/common/theme-head-styles.blade.php
+++ resources/views/UI-FRONTEND/common/theme-head-styles.blade.php
@@ -40,7 +40,7 @@
     <link rel="stylesheet" href="100/531/894/themes/1018832/assets/custom.css?ww-skel-title-6" media="all">
     <link
       rel="stylesheet"
-      href="100/531/894/themes/1018832/assets/quickview.css?ww-qv-btn-bg-50"
+      href="100/531/894/themes/1018832/assets/quickview.css?ww-qv-btn-bold-circle-2"
       media="print"
       onload="this.media='all'"
     >
@@ -52,7 +52,7 @@
     >
     <noscript>
       <link
-        href="100/531/894/themes/1018832/assets/quickview.css?ww-qv-btn-bg-50"
+        href="100/531/894/themes/1018832/assets/quickview.css?ww-qv-btn-bold-circle-2"
         rel="stylesheet"
         type="text/css"
         media="all"
